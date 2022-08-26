2a70759b79547834cedf3d6ae3628236e8106ac1
Nicola Caddeo <nicola.caddeo58@gmail.com>
2022-08-26 10:47:19 +0200

    added option to print numbers in HTML

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 // variabili
 let i;
-
+const appContainer = document.getElementById('app-container');
 // ciclo
 
 for (i = 1; i <= 100; i++) {
-    let fizzModulus = (i) % 3;
-    let buzzModulus = (i) % 5;
+    let fizzModulus = i % 3;
+    let buzzModulus = i % 5;
+    const element = document.createElement('div');
+    if ((fizzModulus == 0) && (buzzModulus == 0)) {
+        element.append('FizzBuzz');
+    } else if (fizzModulus == 0) {
+        element.append('Fizz');
+    } else if (buzzModulus == 0) {
+        element.append('Buzz');
+    } else {
+        element.append(i);
+    }
+    appContainer.append(element);
+
+}
+
+// algoritmo per stampare solo in console 
+/*
+for (i = 1; i <= 100; i++) {
+    let fizzModulus = i % 3;
+    let buzzModulus = i % 5;
 
     if ((fizzModulus == 0) && (buzzModulus == 0)) {
         console.log('FizzBuzz');
@@ -18,3 +37,8 @@ for (i = 1; i <= 100; i++) {
     }
 
 }
+*/
+
+
+
+
